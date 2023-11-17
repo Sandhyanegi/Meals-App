ba6bf74bdd2ba5034fbbcff520854141bdd5ee4d
Sandhyanegigit config --global user.email snsandhya9923@gmail.comgit config --global user.name Sandhyanegigit config --global user.name Sandhyanegi <snsandhya9923@gmail.com>
2023-11-17 18:22:43 +0530

    added drawer navigation

--- mealsApp/App.js
+++ mealsApp/App.js
@@ -1,3 +1,4 @@
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import { StyleSheet} from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
@@ -5,8 +6,44 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavouriteScreen from './screens/FavouriteScreen';
+import {Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
+const Drawer =createDrawerNavigator();
+
+function DrawerNavigator(){
+  return <Drawer.Navigator 
+                screenOptions = {{
+                  headerStyle: {backgroundColor:'#351401'},
+                  headerTintColor:'white',
+                  sceneContainerStyle: {backgroundColor: '#3f2f25',
+                  drawerContentStyle :{backgroundColor:'#351401'} ,
+                  drawerInactiveTintColor: 'white',
+                  drawerActiveBackgroundColor: '#e4baal',
+                 }
+              }}
+          >
+
+            <Drawer.Screen 
+              name="Categories" 
+              component={CategoriesScreen} 
+              options={{
+                title:'All Categories',
+                drawerIcon: ({color,size}) => ( <Ionicons name="list"  color={color} size={size} /> ),
+                }} 
+            />
+
+            <Drawer.Screen 
+              name="Favourites" 
+              component={FavouriteScreen} 
+              options={{
+                drawerIcon: ({color,size}) => ( <Ionicons name="star"  color={color} size={size} /> ),
+                }} 
+            />
+         </Drawer.Navigator>;
+
+}
 
 export default function App() {
   return (
@@ -19,24 +56,26 @@ export default function App() {
               headerTintColor:'white',
               contentStyle: {backgroundColor: '#3f2f25'}
             }}>
-          <Stack.Screen 
-            name="Meals Categories" 
-            component={CategoriesScreen} 
+
+          <Stack.Screen                           //1
+            name="Drawer" 
+            component={DrawerNavigator} 
             options = {{
-              title : 'All Categories',            
+              headerShown:false,            
             }} 
           />
-          <Stack.Screen 
+
+          <Stack.Screen                           //2
             name="Meals Overview" 
             component={MealsOverviewScreen} 
-            // options = {(route) => {
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title:catId,
-            //   };
-            // }}
           />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+
+          <Stack.Screen                             //3
+            name="MealDetail" 
+            component={MealDetailScreen}
+            options={{title:'About the Meal'}}
+          />
+
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ mealsApp/screens/FavouriteScreen.js
@@ -0,0 +1,7 @@
+import{Text} from 'react-native';
+
+function FavouriteScreen(params) {
+    return <Text>favourite  screen </Text>
+}
+
+export default FavouriteScreen;
